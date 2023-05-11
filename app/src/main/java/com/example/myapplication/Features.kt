81976d96ed86dfa4ce10ebc9ffbea8bbefc387f0
smilole <levkar787@gmail.com>
2023-05-12 00:16:00 +0700

    Списал функции с интернетных, что-то доделал, как-то работает перетаскивание блоков

--- app/src/main/java/com/example/myapplication/Features.kt
+++ app/src/main/java/com/example/myapplication/Features.kt
@@ -4,27 +4,35 @@ import android.content.Context
 import android.util.DisplayMetrics
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlin.math.abs
 import kotlin.math.roundToInt
 
-fun convertPixelsToDp(px: Float, context: Context): Float {
+/*fun convertPixelsToDp(px: Float, context: Context): Float {
     return px / (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
 }
 @Composable
@@ -110,4 +118,200 @@ fun drawBlock(
         }
     }
 }
+*/
 
+/*
+    LazyListItemInfo.index is the item's absolute index in the list
+
+    Based on the item's "relative position" with the "currently top" visible item,
+    this returns LazyListItemInfo corresponding to it
+*/
+fun LazyListState.getVisibleItemInfoFor(absoluteIndex: Int): LazyListItemInfo? {
+    return this.layoutInfo.visibleItemsInfo.getOrNull(absoluteIndex - this.layoutInfo.visibleItemsInfo.first().index)
+}
+
+/*
+  Bottom offset of the element in Vertical list
+*/
+val LazyListItemInfo.offsetEnd: Int
+    get() = this.offset + this.size
+
+/*
+   Moving element in the list
+*/
+fun <T> MutableList<T>.move(from: Int, to: Int) {
+    if (from == to)
+        return
+
+    val element = this.removeAt(from) ?: return
+    this.add(to, element)
+}
+@Composable
+fun rememberReorderableListState(
+    lazyListState: LazyListState = rememberLazyListState(),
+    onMove: (Int, Int) -> Unit,
+): ReorderableListState {
+    return remember { ReorderableListState(lazyListState = lazyListState, onMove = onMove) }
+}
+
+class ReorderableListState(
+    val lazyListState: LazyListState,
+    private val onMove: (Int, Int) -> Unit
+) {
+    var draggedDistance by mutableStateOf(0f)
+
+    // used to obtain initial offsets on drag start
+    var initiallyDraggedElement by mutableStateOf<LazyListItemInfo?>(null)
+
+    var currentIndexOfDraggedItem by mutableStateOf<Int?>(null)
+
+    val initialOffsets: Pair<Int, Int>?
+        get() = initiallyDraggedElement?.let { Pair(it.offset, it.offsetEnd) }
+
+    val elementDisplacement: Float?
+        get() = currentIndexOfDraggedItem
+            ?.let { lazyListState.getVisibleItemInfoFor(absoluteIndex = it) }
+            ?.let { item -> (initiallyDraggedElement?.offset ?: 0f).toFloat() + draggedDistance - item.offset }
+
+    val currentElement: LazyListItemInfo?
+        get() = currentIndexOfDraggedItem?.let {
+            lazyListState.getVisibleItemInfoFor(absoluteIndex = it)
+        }
+
+    var overscrollJob by mutableStateOf<Job?>(null)
+
+    fun onDragStart(offset: Offset) {
+        lazyListState.layoutInfo.visibleItemsInfo
+            .firstOrNull { item -> offset.y.toInt() in item.offset..(item.offset + item.size) }
+            ?.also {
+                currentIndexOfDraggedItem = it.index
+                initiallyDraggedElement = it
+            }
+    }
+
+    fun onDragInterrupted() {
+        draggedDistance = 0f
+        currentIndexOfDraggedItem = null
+        initiallyDraggedElement = null
+        overscrollJob?.cancel()
+    }
+
+    fun onDrag(offset: Offset) {
+        draggedDistance += offset.y
+
+        initialOffsets?.let { (topOffset, bottomOffset) ->
+            val startOffset = topOffset + draggedDistance
+            val endOffset = bottomOffset + draggedDistance
+
+            currentElement?.let { hovered ->
+                lazyListState.layoutInfo.visibleItemsInfo
+                    .filterNot { item -> item.offsetEnd < startOffset || item.offset > endOffset || hovered.index == item.index }
+                    .firstOrNull { item ->
+                        val delta = startOffset - hovered.offset
+                        when {
+                            delta > 0 -> (endOffset > item.offsetEnd)
+                            else -> (startOffset < item.offset)
+                        }
+                    }
+                    ?.also { item ->
+                        currentIndexOfDraggedItem?.let { current -> onMove.invoke(current, item.index) }
+                        currentIndexOfDraggedItem = item.index
+                    }
+            }
+        }
+    }
+
+    fun checkForOverScroll(): Float {
+        return initiallyDraggedElement?.let {
+            val startOffset = it.offset + draggedDistance
+            val endOffset = it.offsetEnd + draggedDistance
+
+            return@let when {
+                draggedDistance > 0 -> (endOffset - lazyListState.layoutInfo.viewportEndOffset).takeIf { diff -> diff > 0 }
+                draggedDistance < 0 -> (startOffset - lazyListState.layoutInfo.viewportStartOffset).takeIf { diff -> diff < 0 }
+                else -> null
+            }
+        } ?: 0f
+    }
+}
+
+@Composable
+fun ReorderableList(
+    items: List<ReorderItem>,
+    onMove: (Int, Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    val scope = rememberCoroutineScope()
+
+    var overscrollJob by remember { mutableStateOf<Job?>(null) }
+
+    val reorderableListState = rememberReorderableListState(onMove = onMove)
+
+    LazyColumn(
+        modifier = modifier
+            .pointerInput(Unit) {
+                detectDragGesturesAfterLongPress(
+                    onDrag = { change, offset ->
+                        change.consume()
+                        reorderableListState.onDrag(offset)
+
+                        if (overscrollJob?.isActive == true)
+                            return@detectDragGesturesAfterLongPress
+
+                        reorderableListState.checkForOverScroll()
+                            .takeIf { it != 0f }
+                            ?.let { overscrollJob = scope.launch { reorderableListState.lazyListState.scrollBy(it) } }
+                            ?: run { overscrollJob?.cancel() }
+                    },
+                    onDragStart = { offset -> reorderableListState.onDragStart(offset) },
+                    onDragEnd = { reorderableListState.onDragInterrupted() },
+                    onDragCancel = { reorderableListState.onDragInterrupted() }
+                )
+            }
+            .fillMaxWidth()
+            .fillMaxHeight(0.8f)
+            .background(Color.LightGray),
+        state = reorderableListState.lazyListState,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        itemsIndexed(items) { index, item ->
+            Column(
+                modifier = Modifier
+                    .composed {
+                        val offsetOrNull =
+                            reorderableListState.elementDisplacement.takeIf {
+                                index == reorderableListState.currentIndexOfDraggedItem
+                            }
+
+                        Modifier
+                            .graphicsLayer {
+                                translationY = offsetOrNull ?: 0f
+                            }
+                    }
+                    .height(70.dp)
+                    .background(Color.White, shape = RoundedCornerShape(4.dp))
+                    .fillMaxWidth()
+            ) { Text(text = "Item ${item.id}") }
+        }
+    }
+}
+@Composable
+fun Screen() {
+    val list = listOf(ReorderItem(1), ReorderItem(2),ReorderItem(1), ReorderItem(2),ReorderItem(1), ReorderItem(2),ReorderItem(1), ReorderItem(2),ReorderItem(1), ReorderItem(2),ReorderItem(1), ReorderItem(2)).toMutableStateList()
+
+    Column {
+        ReorderableList(
+            items = list,
+            onMove = { fromIndex, toIndex -> list.move(fromIndex, toIndex) }
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .horizontalScroll(rememberScrollState())
+                .background(Color.DarkGray)
+        ){
+
+        }
+    }
+}
