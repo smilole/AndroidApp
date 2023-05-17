3ce1274a2a388cb7115334044339d01254e764e4
Dmitriy Vasilev <dima.vasiliev2002@mail.ru>
2023-05-17 17:18:46 +0700

    dada

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.myapplication.screens.Screen
 
 
 class MainActivity : ComponentActivity() {
